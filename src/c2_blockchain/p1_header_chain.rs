c405019fa8528a9a0f70d72bb4295da890feb50a
trantorian <114066155+Trantorian1@users.noreply.github.com>
2024-05-27 12:02:43 +0200

    feat(c2p1): finished c2/p1

--- src/c2_blockchain/p1_header_chain.rs
+++ src/c2_blockchain/p1_header_chain.rs
@@ -3,6 +3,10 @@
 //! start with that.
 //!
 
+use std::hash::{DefaultHasher, Hash as _, Hasher};
+
+use itertools::{FoldWhile, Itertools};
+
 use crate::hash;
 
 // We will use Rust's built-in hashing where the output type is u64. I'll make an alias
@@ -25,12 +29,27 @@ pub struct Header {
 impl Header {
     /// Returns a new valid genesis header.
     fn genesis() -> Self {
-        todo!("Exercise 1")
+        Self {
+            parent: Hash::default(),
+            height: 0,
+            extrinsics_root: (),
+            state_root: (),
+            consensus_digest: (),
+        }
     }
 
     /// Create and return a valid child header.
     fn child(&self) -> Self {
-        todo!("Exercise 2")
+        let mut hasher = DefaultHasher::new();
+        self.hash(&mut hasher);
+
+        Self {
+            parent: hasher.finish(),
+            height: self.height + 1,
+            extrinsics_root: (),
+            state_root: (),
+            consensus_digest: (),
+        }
     }
 
     /// Verify that all the given headers form a valid chain from this header to the tip.
@@ -38,7 +57,23 @@ impl Header {
     /// This method may assume that the block on which it is called is valid, but it
     /// must verify all of the blocks in the slice;
     fn verify_sub_chain(&self, chain: &[Header]) -> bool {
-        todo!("Exercise 3")
+        chain
+            .iter()
+            .fold_while((true, self), |(_, parent), header| {
+                let mut hasher = DefaultHasher::new();
+                parent.hash(&mut hasher);
+
+                let is_valid_hash = hasher.finish() == header.parent;
+                let is_valid_height = header.height == parent.height + 1;
+
+                if !is_valid_hash || !is_valid_height {
+                    FoldWhile::Done((false, header))
+                } else {
+                    FoldWhile::Continue((true, header))
+                }
+            })
+            .into_inner()
+            .0
     }
 }
 
@@ -46,14 +81,22 @@ impl Header {
 
 /// Build and return a valid chain with exactly five blocks including the genesis block.
 fn build_valid_chain_length_5() -> Vec<Header> {
-    todo!("Exercise 4")
+    let genesis = Header::genesis();
+
+    (0..5).into_iter().fold(vec![genesis], |mut acc, _| {
+        acc.push(acc.last().unwrap().child());
+        acc
+    })
 }
 
 /// Build and return a chain with at least three headers.
 /// The chain should start with a proper genesis header,
 /// but the entire chain should NOT be valid.
 fn build_an_invalid_chain() -> Vec<Header> {
-    todo!("Exercise 5")
+    let mut invalid_chain = build_valid_chain_length_5();
+    invalid_chain[3] = Header::genesis();
+
+    invalid_chain
 }
 
 // To run these tests: `cargo test bc_1
